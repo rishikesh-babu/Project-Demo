c1cb09e187148be76d417feb0e02722c73caa5ba
rishikesh-babu <rishikeshbabu2004@gmail.com>
2025-01-01 18:18:15 +0530

    Add frontend link to cors

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ env.config()
 const port = 3000
 const app = express()
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://project-demo-frontend.onrender.com'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE']
 }))
